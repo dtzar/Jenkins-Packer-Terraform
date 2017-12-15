90d5ad44324d31f684e0eff9b1bd92849a0474a1
Thomas Stringer <github@trstringer.com>
2017-12-15 17:06:41 -0500

    (wip) add vmss deployment to tf config

--- httpd/main.tf
+++ httpd/main.tf
@@ -5,12 +5,48 @@ provider "azurerm" {
   tenant_id       = "${var.tenant_id}"
 }
 
-resource "azurerm_resource_group" "testrg" {
-  name     = "apache"
-  location = "eastus"
-}
-
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
+
+variable "rg_name" {
+  default = "apache2"
+}
+
+resource "azurerm_resource_group" "testrg" {
+  name     = "${var.rg_name}"
+  location = "eastus"
+}
+
+module "network" {
+  source = "Azure/network/azurerm"
+  resource_group_name = "${var.rg_name}"
+  location = "eastus"
+}
+
+module "loadbalancer" {
+  source = "Azure/loadbalancer/azurerm"
+  resource_group_name = "${var.rg_name}"
+  location = "eastus"
+  prefix = "tflbtest"
+  lb_port = {
+    http = [ "80", "Tcp", "80" ]
+  }
+}
+
+module "computegroup" {
+  source = "Azure/computegroup/azurerm"
+  resource_group_name = "${var.rg_name}"
+  location = "eastus"
+  load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
+  vnet_subnet_id = "${module.network.vnet_subnets[0]}"
+  lb_port = {
+    http = [ "80", "Tcp", "80" ]
+    https = [ "443", "Tcp", "443" ]
+  }
+  vm_size = "Standard_A0"
+  vm_os_publisher = "OpenLogic"
+  vm_os_offer = "CentOS"
+  vm_os_sku = "7.4"
+}

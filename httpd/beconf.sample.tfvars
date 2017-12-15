1afeff46450a16bfc1df1255668fafd28e2d3bf0
Thomas Stringer <github@trstringer.com>
2017-12-15 13:19:55 -0500

    (wip) add terraform cd config

---
+++ httpd/beconf.sample.tfvars
@@ -0,0 +1,4 @@
+storage_account_name = "..."
+container_name = "..."
+access_key = "..."
+key = "test.terraform.state"
--- httpd/set_env_vars.sample.sh
+++ httpd/set_env_vars.sample.sh
@@ -6,3 +6,8 @@ export AZURE_SUBSCRIPTION_ID=
 export IMAGE_NAME=
 export IMAGE_RESOURCE_GROUP_NAME=
 export BUILD_RESOURCE_GROUP_NAME=
+
+export TF_VAR_subscription_id=$AZURE_SUBSCRIPTION_ID
+export TF_VAR_tenant_id=
+export TF_VAR_client_id=$AZURE_CLIENT_ID
+export TF_VAR_client_secret=$AZURE_CLIENT_SECRET

f65aca7591f348d1e09a90b2d9a1822bdb02a3cc
David Tesar <dtzar@hotmail.com>
2017-12-18 11:19:59 -0800

    add location var

--- httpd/main.tf
+++ httpd/main.tf
@@ -18,6 +18,10 @@ variable "packer_image" {
   default = ""
 }
 
+variable "location"{
+  default = "eastus"
+}
+
 resource "azurerm_resource_group" "testrg" {
   name     = "${var.rg_name}"
   location = "${var.location}"
